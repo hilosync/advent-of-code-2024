99ae0990e67b1f68d00b9bcc18054352fdf2ff87
Niks Ronis <niksronis@gmail.com>
2024-12-04 15:41:08 +0000

    Day 1 part 2 solution

---
+++ day1/part2.py
@@ -0,0 +1,25 @@
+input = open('input.txt')
+
+list1 = []
+list2 = []
+
+for line in input:
+    item1, item2 = line.split("   ")
+
+    list1.append(int(item1))
+    list2.append(int(item2))
+
+occurenceCount = {}
+
+for i in list2:
+    if i not in  occurenceCount:
+        occurenceCount[i] = 0
+    occurenceCount[i] += 1
+
+similarityScore = 0
+
+for i in list1:
+    if i in occurenceCount:
+        similarityScore += i * occurenceCount[i]
+
+print(similarityScore)
